f2f1772db61c8ce02f35a7711b4f6eb3eff467a2
ljj33 <ljj3347@naver.com>
2023-09-08 10:55:21 +0900

    40th commit

--- components/Parliament.js
+++ components/Parliament.js
@@ -15,6 +15,9 @@ export default function Parliament() {
   const [minjuarticle,setMinjuarticle] = useState([]);
   const [minjuarticleLoading, setMinjuarticleLoading] = useState(true);
 
+  const [parliamentCreator,setParliamentCreator] = useState([]);
+  const [parliamentCreatorLoading, setParliamentCreatorLoading] = useState(true);
+
 
   useEffect(() => {
     // 데이터를 가져오는 함수를 정의합니다.
@@ -38,9 +41,21 @@ export default function Parliament() {
         console.error('Error fetching data:', error);
       }
     };
+    const fetchData5 = async () => {
+      try {
+        const response = await axios.get(`https://mks5ux6whggik4anhr3c5ofdie0abvss.lambda-url.ap-northeast-2.on.aws/getParliamentCreator?page=1`);
+        setParliamentCreator(response.data);
+        setParliamentCreatorLoading(false);
+        console.log("loading완료5")
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
 
     fetchData3(); // 함수를 호출하여 데이터를 가져옵니다.
     fetchData4(); // 함수를 호출하여 데이터를 가져옵니다.
+    fetchData5(); // 함수를 호출하여 데이터를 가져옵니다.
   }, []);
 
   return (
@@ -59,26 +74,76 @@ export default function Parliament() {
       
       <div className="px-20 pt-10 grid gap-8 md:grid-cols-2">
         <div>
-        <h1 className='mb-5 text-2xl font-bold text-center'>국민의힘</h1>
-          <div className="relative overflow-x-auto shadow-md sm:rounded-lg bg-white">  
+          <h1 className='mb-5 text-2xl font-bold text-center'>국민의힘</h1>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg bg-white">  
+              <div>
+                <table className="w-full h-full text-sm text-left text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                      <tr>
+                          <th scope="col" className="px-6 py-3">
+                              제목
+                          </th>
+                          <th scope="col" className="px-6 py-3">
+                              카테고리
+                          </th>
+                          <th scope="col" className="px-6 py-3">
+                              작성일
+                          </th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {
+                        gukhimarticle.map((elem,index)=>{
+                          return (
+                          <tr key={index} className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                            <th scope="row" className="inline-block w-[400px] truncate px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                <a target='_blank' href={elem.url}>{elem['title']}</a>
+                            </th>
+                            <td className="w-1/4 px-6 py-4">
+                                {elem['category']}
+                            </td>
+                            <td className="w-1/4 px-6 py-4">
+                                {elem['regiDate']}
+                            </td>
+                        </tr>
+                          )
+                        })
+                      }
+
+                      
+                  </tbody>
+                </table>
+                <div className='text-right my-3 pr-5'>
+                  <p className="text-base font-semibold text-blue-500 text-right"><Link target='_blank' className='z-50' href='https://www.peoplepowerparty.kr/news/comment/BBSDD0001'>Read more</Link></p>
+                </div>  
+              </div>
+            </div>
+          </div>
+
+
+
+        <div>
+          <h1 className='mb-5 text-2xl font-bold text-center'>더불어민주당</h1>
+          <div className="relative overflow-x-auto shadow-md sm:rounded-lg bg-white">
+            
             <div>
               <table className="w-full h-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="px-6 py-3">
-                            제목
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            카테고리
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            작성일
-                        </th>
-                    </tr>
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                        제목
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        카테고리
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        작성일
+                    </th>
+                  </tr>
                 </thead>
-                <tbody>
+                  <tbody>
                     {
-                      gukhimarticle.map((elem,index)=>{
+                      minjuarticle.map((elem,index)=>{
                         return (
                         <tr key={index} className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
                           <th scope="row" className="inline-block w-[400px] truncate px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -94,61 +159,61 @@ export default function Parliament() {
                         )
                       })
                     }
-
-                    
-                </tbody>
+                  </tbody>
               </table>
               <div className='text-right my-3 pr-5'>
-                <p className="text-base font-semibold text-blue-500 text-right"><Link target='_blank' className='z-50' href='https://www.peoplepowerparty.kr/news/comment/BBSDD0001'>Read more</Link></p>
+                    <p className="text-base font-semibold text-blue-500 text-right"><Link target='_blank' className='z-50' href='https://theminjoo.kr/main/sub/news/list.php?brd=11'>Read more</Link></p>
               </div>  
-            </div>
+            </div>                 
           </div>
+      
         </div>
         <div>
-        <h1 className='mb-5 text-2xl font-bold text-center'>더불어민주당</h1>
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg bg-white">
-          
-          <div>
-            <table className="w-full h-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" className="px-6 py-3">
-                      제목
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                      카테고리
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                      작성일
-                  </th>
-                </tr>
-              </thead>
-                <tbody>
-                  {
-                    minjuarticle.map((elem,index)=>{
-                      return (
-                      <tr key={index} className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                        <th scope="row" className="inline-block w-[400px] truncate px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            <a target='_blank' href={elem.url}>{elem['title']}</a>
-                        </th>
-                        <td className="w-1/4 px-6 py-4">
-                            {elem['category']}
-                        </td>
-                        <td className="w-1/4 px-6 py-4">
-                            {elem['regiDate']}
-                        </td>
-                    </tr>
-                      )
-                    })
-                  }
-                </tbody>
-            </table>
-          <div className='text-right my-3 pr-5'>
-                <p className="text-base font-semibold text-blue-500 text-right"><Link target='_blank' className='z-50' href='https://theminjoo.kr/main/sub/news/list.php?brd=11'>Read more</Link></p>
-          </div>  
-        </div>                 
-      </div>
-      </div>
+          <h1 className='mb-5 text-2xl font-bold text-center'>국회입법조사처</h1>
+          <div className="relative overflow-x-auto shadow-md sm:rounded-lg bg-white">
+            
+            <div>
+              <table className="w-full h-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                        제목
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        작성자
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        작성일
+                    </th>
+                  </tr>
+                </thead>
+                  <tbody>
+                    {
+                      parliamentCreator.map((elem,index)=>{
+                        return (
+                        <tr key={index} className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                          <th scope="row" className="inline-block w-[400px] truncate px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                              <a target='_blank' href={elem.url}>{elem['title']}</a>
+                          </th>
+                          <td className="w-1/4 px-6 py-4">
+                              {elem['writer']}
+                          </td>
+                          <td className="w-1/4 px-6 py-4">
+                              {elem['regiDate']}
+                          </td>
+                      </tr>
+                        )
+                      })
+                    }
+                  </tbody>
+              </table>
+              <div className='text-right my-3 pr-5'>
+                    <p className="text-base font-semibold text-blue-500 text-right"><Link target='_blank' className='z-50' href='https://www.nars.go.kr/report/list.do?cmsCode=CM0043'>Read more</Link></p>
+              </div>  
+            </div>                 
+         </div>
+      
+        </div>
     </div>
 
     
