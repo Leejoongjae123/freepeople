d7f41497e1bf9ddab92020886b289abfd0a15a5d
ljj33 <ljj3347@naver.com>
2023-09-11 12:56:27 +0900

    56th commit

--- components/SliderKOSIS.js
+++ components/SliderKOSIS.js
@@ -37,10 +37,15 @@ export default function SliderKOSIS() {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 4,
-      slidesToSlide: 4 // optional, default to 1.
+      slidesToSlide: 1 // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 4,
+      slidesToSlide: 1 // optional, default to 1.
     },
     mobile: {
-      breakpoint: { max: 767, min: 464 },
+      breakpoint: { max: 464, min: 0 },
       items: 1,
       slidesToSlide: 1 // optional, default to 1.
     }
