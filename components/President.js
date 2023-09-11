2d0e9ed43a709314d8da5aeee7ccf9217fac0906
ljj33 <ljj3347@naver.com>
2023-09-11 09:56:05 +0900

    51th commit

--- components/President.js
+++ components/President.js
@@ -51,6 +51,16 @@ export default function President() {
       console.error('Error fetching data:', error);
     }
   };
+  const fetchData5 = async () => {
+    try {
+      const response = await axios.get(`https://mks5ux6whggik4anhr3c5ofdie0abvss.lambda-url.ap-northeast-2.on.aws/getEconomyStatus`);
+      setEconomyIndicators(response.data);
+      setEconomyIndicatorsLoading(false);
+      console.log("loading완료5")
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
   // const fetchData3 = async () => {
   //   try {
   //     const response = await fetch(`https://mks5ux6whggik4anhr3c5ofdie0abvss.lambda-url.ap-northeast-2.on.aws/getpresidentbrief?page=1`);
@@ -103,7 +113,7 @@ export default function President() {
     fetchData2(); // 함수를 호출하여 데이터를 가져옵니다.
     // fetchData3(); // 함수를 호출하여 데이터를 가져옵니다.
     // fetchData4(); // 함수를 호출하여 데이터를 가져옵니다.
-    // fetchData5(); // 함수를 호출하여 데이터를 가져옵니다.
+    fetchData5(); // 함수를 호출하여 데이터를 가져옵니다.
   }, []);
 
 
@@ -211,6 +221,92 @@ export default function President() {
           </div>                 
         
         </div>
+        <div className="flex flex-col mx-20">
+          <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 px-18">
+            <div className="flex items-start rounded-xl bg-white p-4 shadow-lg">
+              <div className="flex h-12 w-12 items-center justify-center rounded-full border border-blue-100 bg-blue-50">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                </svg> */}
+                <FcCurrencyExchange size='40'></FcCurrencyExchange>
+              </div>
+
+              <div className="ml-4">
+                <h2 className="font-semibold">환율(USD)</h2>
+                <p className="mt-2 text-sm text-gray-500">{economyIndicators.exchange} 원/달러</p>
+              </div>
+              
+            </div>
+            <div className="flex items-start rounded-xl bg-white p-4 shadow-lg">
+              <div className="flex h-12 w-12 items-center justify-center rounded-full border border-blue-100 bg-blue-50">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                </svg> */}
+                <FcBullish size='40'></FcBullish>
+              </div>
+
+              <div className="ml-4">
+                <h2 className="font-semibold">코스피</h2>
+                <p className="mt-2 text-sm text-gray-500">{economyIndicators.kospi}</p>
+              </div>
+              
+            </div>
+            <div className="flex items-start rounded-xl bg-white p-4 shadow-lg">
+              <div className="flex h-12 w-12 items-center justify-center rounded-full border border-blue-100 bg-blue-50">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                </svg> */}
+                <FcAreaChart size='40'></FcAreaChart>
+              </div>
+
+              <div className="ml-4">
+                <h2 className="font-semibold">코스닥</h2>
+                <p className="mt-2 text-sm text-gray-500">{economyIndicators.kosdaq}</p>
+              </div>
+              
+            </div>
+
+            <div className="flex items-start rounded-xl bg-white p-4 shadow-lg">
+              <div className="flex h-12 w-12 items-center justify-center rounded-full border border-orange-100 bg-orange-50">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-orange-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                </svg> */}
+                <GiBarrel size='40'></GiBarrel>
+              </div>
+
+              <div className="ml-4">
+                <h2 className="font-semibold">유가(두바이)</h2>
+                <p className="mt-2 text-sm text-gray-500">{economyIndicators.oil} 달러/배럴</p>
+              </div>
+            </div>
+            <div className="flex items-start rounded-xl bg-white p-4 shadow-lg">
+              <div className="flex h-12 w-12 items-center justify-center rounded-full border border-red-100 bg-red-50">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                </svg> */}
+                <AiFillGolden color='#FFd700' size='40'></AiFillGolden>
+              </div>
+
+              <div className="ml-4">
+                <h2 className="font-semibold">금 시세</h2>
+                <p className="mt-2 text-sm text-gray-500">{economyIndicators.gold} 달러/t oz</p>
+              </div>
+            </div>
+            <div className="flex items-start rounded-xl bg-white p-4 shadow-lg">
+              <div className="flex h-12 w-12 items-center justify-center rounded-full border border-indigo-100 bg-indigo-50">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-indigo-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                </svg> */}
+                <FcMoneyTransfer  size='40'></FcMoneyTransfer>
+              </div>
+
+              <div className="ml-4">
+                <h2 className="font-semibold">CD금리(91일)</h2>
+                <p className="mt-2 text-sm text-gray-500">{economyIndicators.cdinterest} %/년</p>
+              </div>
+            </div>
+          </div>
+        </div>        
 
       </section>
     </>
